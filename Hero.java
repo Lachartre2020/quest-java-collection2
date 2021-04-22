973ab93336cb27c5719cfb3da9c39563a6ec3238
Lachartre2020 <s.lachartre@hotmail.fr>
2021-04-23 00:22:14 +0200

    Implementation sort

---
+++ Hero.java
@@ -0,0 +1,48 @@
+package questjavacollection2;
+
+import java.util.Comparator;
+
+public class Hero implements Comparable<Hero>{
+
+    // attributs
+    private String name;
+    private int age;
+
+    // constructeurs
+    public Hero(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // accesseurs (getters)
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // mutateurs (setters)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public static Comparator<Hero> comparator = new Comparator<Hero>(){
+        @Override
+        public int compare(Hero hero1, Hero hero2) {
+        	return  hero2.getAge()-hero1.getAge();
+        }
+    };
+        
+    @Override
+    public int compareTo(Hero hero) {
+    	return this.getName().compareTo(hero.getName());
+    }
+    
+    
+}
---
+++ Thanos.java
@@ -0,0 +1,37 @@
+package questjavacollection2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Thanos {
+
+    public static void main(String[] args) {
+
+        List<Hero> heroes = new ArrayList<>();
+        heroes.add(new Hero("Hulk", 49));
+        heroes.add(new Hero("Black Widow", 34));
+        heroes.add(new Hero("Captain America", 100));
+        heroes.add(new Hero("Thor", 1501));
+
+        // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
+
+        Collections.sort(heroes);
+        
+        System.out.println("\nOrder by name:");
+        showList(heroes);
+
+        // TODO 2: Add a Comparator and sort by age (descending)
+    	Collections.sort(heroes, Hero.comparator);
+
+        System.out.println("\nOrder by age:");
+        showList(heroes);
+    }
+    
+    private static void showList(List<Hero> heroes) {
+        for (Hero heroe : heroes) {
+            System.out.println(heroe.getName() + ", " + heroe.getAge());
+        }
+    }
+}
